a506079fc24b7d2420cd0ae0e495d20c7c0db9de
Andrii Reverchuk <andrejrevercuk@gmail.com>
2021-05-28 18:35:56 +0300

    view chat

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,25 @@ import { io } from "socket.io-client";
 const WS_URL = "ws://bt-21-playground-vppfc.ondigitalocean.app/";
 
 const socket = io(WS_URL);
-
+const connectedUsers: Record<string, string> = {};
 socket.on("connect", () => {
    const chat = document.getElementById("chat");
    socket.on("new_message", (username, message) => {
+      const date = new Date();
       const messageContainer = document.createElement("div");
       messageContainer.classList.add("message-container");
       const userContainer = document.createElement("p");
       const messageText = document.createElement("p");
-      userContainer.textContent = username + " :";
+      userContainer.textContent =
+         date.getHours() + ":" + date.getMinutes() + " " + username + " :";
       messageText.textContent = message;
       messageContainer.append(userContainer, messageText);
       chat.append(messageContainer);
       console.log(`${username} ${message}`);
+   });
+   socket.on("new_message", (username, message, userId) => {
+      connectedUsers[userId] = username;
+      console.log(userId);
    });
    socket.emit("get_users");
    socket.on("user_list", (users) => {
@@ -26,13 +32,15 @@ socket.on("connect", () => {
 const name: string = "Andrii";
 const btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
+   const date = new Date();
    const text = (<HTMLInputElement>document.getElementById("message")).value;
    const chat = document.getElementById("chat");
    const messageContainer = document.createElement("div");
    messageContainer.classList.add("message-container");
    const userContainer = document.createElement("p");
    const messageText = document.createElement("p");
-   userContainer.textContent = name + " :";
+   userContainer.textContent =
+      date.getHours() + ":" + date.getMinutes() + " " + name + " : ";
    messageText.textContent = text;
    messageContainer.append(userContainer, messageText);
    chat.append(messageContainer);
